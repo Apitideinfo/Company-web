5e86c8bde818d65b13affa6d23fecc38731d83b4
mayank-joshii <joshi.mayank6@gmail.com>
2025-07-30 12:38:01 +0530

    new deploy

--- apitide_website/settings.py
+++ apitide_website/settings.py
@@ -78,10 +78,11 @@ WSGI_APPLICATION = 'apitide_website.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
+        conn_max_age=600,
+        ssl_require=True,
+        default=os.environ.get('DATABASE_URL')
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
